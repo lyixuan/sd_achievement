ed4ca8a086b0046550de5f02963c451df332803a
wangjing <1915093132@qq.com>
2018-08-21 20:13:41 +0800

    tab组件封装提交

--- src/routes/Home/Teachers/family.jsx
+++ src/routes/Home/Teachers/family.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'dva';
 import { assignUrlParams } from '../../../utils/routerUtils';
 import { getCurrentAuthInfo } from '../../../utils/localStorage';
+import LineChartTab from '../../../components/SelfTab/LineChartTab';
+import PerformanceTab from '../../../components/SelfTab/PerformanceTab';
 
 class Boss extends React.Component {
   constructor(props) {
@@ -11,6 +13,8 @@ class Boss extends React.Component {
       paramsObj: {
         startTime: null, // 过滤开始时间
       },
+      flag: 1,
+      flag2: 1,
     };
     this.state = assignUrlParams(initState, urlParams);
   }
@@ -21,7 +25,27 @@ class Boss extends React.Component {
   };
 
   render() {
-    return <div>家族长首页,权限是:{this.checkoutUserAuth()}</div>;
+    return (
+      <div>
+        <span>家族长首页,权限是:{this.checkoutUserAuth()}</span>
+        <div style={{ marginTop: '1rem' }}>
+          <LineChartTab
+            firstId={this.state.flag}
+            callBackFun={id => {
+              this.setState({ flag: id });
+            }}
+          />
+        </div>
+        <div style={{ marginTop: '2rem' }}>
+          <PerformanceTab
+            firstId={this.state.flag2}
+            callBackFun={id => {
+              this.setState({ flag2: id });
+            }}
+          />
+        </div>
+      </div>
+    );
   }
 }
 export default connect(({ loading }) => ({ loading }))(Boss);
