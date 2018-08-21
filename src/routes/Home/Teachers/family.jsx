b9c817ce8a7ef00e208a43efa3fcc8cddbb90dfb
wangjing <1915093132@qq.com>
2018-08-21 20:55:00 +0800

    时间控件代码提交

--- src/routes/Home/Teachers/family.jsx
+++ src/routes/Home/Teachers/family.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Button } from 'antd-mobile';
 import { assignUrlParams } from '../../../utils/routerUtils';
 import { getCurrentAuthInfo } from '../../../utils/localStorage';
+import styles from './index.less';
 import LineChartTab from '../../../components/SelfTab/LineChartTab';
 import PerformanceTab from '../../../components/SelfTab/PerformanceTab';
+import dateImg from '../../../assets/dateSelect.svg';
+import Dialog from '../../../components/Dialog';
+import ButtonGroup from '../../../components/ButtonGroup/ButtonGroup';
 
 class Boss extends React.Component {
   constructor(props) {
@@ -15,6 +20,7 @@ class Boss extends React.Component {
       },
       flag: 1,
       flag2: 1,
+      dialogVisible: false,
     };
     this.state = assignUrlParams(initState, urlParams);
   }
@@ -24,23 +30,95 @@ class Boss extends React.Component {
     return groupType;
   };
 
+  showModel = bol => {
+    if (bol) {
+      this.choseDateArea();
+      this.setState({ dialogVisible: bol });
+    } else {
+      this.setState({ dialogVisible: bol });
+    }
+  };
+
+  nextStep = () => {
+    this.setState({
+      dialogVisible: false,
+    });
+  };
+
+  choseDateArea = () => {
+    const dateArr = [
+      { id: '2018.08', name: '2018.08' },
+      { id: '2018.07', name: '2018.07' },
+      { id: '2018.06', name: '2018.06' },
+      { id: '2018.05', name: '2018.05' },
+    ];
+    return dateArr;
+  };
+
+  renderGroupList = () => {
+    // 此方法用于render出groupList
+    const data = this.choseDateArea();
+    return (
+      <ButtonGroup
+        dataSource={{ data }}
+        // id={selectedTime}
+        btnClass={styles.timeBtnStyle}
+        btnSelectedClass={styles.timeBtnSelected}
+        dataReturnFun={item => {
+          console.log(item);
+        }}
+      />
+    );
+  };
+
   render() {
+    const { dialogVisible, flag, flag2 } = this.state;
     return (
       <div>
         <span>家族长首页,权限是:{this.checkoutUserAuth()}</span>
-        <div style={{ marginTop: '1rem' }}>
-          <LineChartTab
-            firstId={this.state.flag}
+        <div style={{ marginTop: '1rem', position: 'relative', marginLeft: '0.3rem' }}>
+          <span className={styles.timeName}>时间:</span>
+          <span style={{ marginLeft: '0.1rem' }} className={styles.timeDate}>
+            2018.08.01 - 2018.08.21
+          </span>
+          <img
+            onClick={this.showModel.bind(this, true)}
+            className={styles.timeImg}
+            src={dateImg}
+            alt="时间图片"
+          />
+        </div>
+
+        <div className={styles.download}>
+          {dialogVisible && (
+            <Dialog
+              visible={dialogVisible}
+              showModel={bol => this.showModel(bol)}
+              title={<p className={styles.dialogTitle}>请选择所要下载的底表</p>}
+              modelClass={styles.modelClass}
+              cotainerClass={styles.flexContainer}
+            >
+              <div className={styles.timeList}>{this.renderGroupList()}</div>
+              <Button className={styles.nextStep} onClick={this.nextStep}>
+                下一步
+              </Button>
+            </Dialog>
+          )}
+        </div>
+
+        <div style={{ marginTop: '0.4rem' }}>
+          <PerformanceTab
+            firstId={flag2}
             callBackFun={id => {
-              this.setState({ flag: id });
+              this.setState({ flag2: id });
             }}
           />
         </div>
-        <div style={{ marginTop: '2rem' }}>
-          <PerformanceTab
-            firstId={this.state.flag2}
+        <div style={{ marginTop: '0.4rem' }}>
+          <LineChartTab
+            firstId={flag}
             callBackFun={id => {
-              this.setState({ flag2: id });
+              this.setState({ flag: id });
             }}
           />
         </div>
---
+++ src/utils/fixModel.js
@@ -0,0 +1,74 @@
+function closest(el, selector) {
+  const matchesSelector =
+    el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+  let e = el;
+  while (e) {
+    if (matchesSelector.call(e, selector)) {
+      return e;
+    }
+    e = e.parentElement;
+  }
+  return null;
+}
+
+/**
+ * 修复antd-mobile Modal组件touch事件穿透
+ * 在组件解挂的时候对事件进行remove,提供一个参数removeEvent
+ * 需要滚动的地方请加class: 'scroller'
+ */
+let data = {
+  scroller: null,
+  posY: 0,
+  scrollY: 0,
+  maxscroll: 0,
+};
+function onTouchStart(e) {
+  const scroller = closest(e.target, '.scroller');
+  if (!scroller) return;
+  data.scroller = scroller;
+  // 垂直位置标记
+  data.posY = e.touches[0].pageY;
+  data.scrollY = scroller.scrollTop;
+  // 是否可以滚动
+  data.maxscroll = scroller.scrollHeight - scroller.clientHeight;
+}
+function onTouchmove(e) {
+  if (!data.scroller) {
+    return e.preventDefault();
+  }
+
+  const { scrollTop } = data.scroller;
+  const distanceY = e.touches[0].pageY - data.posY;
+
+  // 上下边缘检测
+  if (distanceY > 0 && scrollTop === 0) {
+    // 往上滑，并且到头
+    // 禁止滚动的默认行为
+    return e.preventDefault();
+  }
+
+  // 下边缘检测
+  if (distanceY < 0 && scrollTop + 1 >= data.maxscroll) {
+    // 往下滑，并且到头
+    // 禁止滚动的默认行为
+    return e.preventDefault();
+  }
+}
+function onTouchend() {
+  data.scroller = null;
+  data.maxscroll = 0;
+}
+export function fixModal(removeEvent = false) {
+  data = {
+    scroller: null,
+    posY: 0,
+    scrollY: 0,
+    maxscroll: 0,
+  };
+
+  const event = removeEvent ? 'removeEventListener' : 'addEventListener';
+  document.body.style.overflow = removeEvent ? 'auto' : 'hiden';
+  document.body[event]('touchstart', onTouchStart, { passive: false });
+  document.body[event]('touchmove', onTouchmove, { passive: false });
+  document.body[event]('touchend', onTouchend, { passive: false });
+}
