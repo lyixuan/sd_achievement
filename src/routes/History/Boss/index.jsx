6e671c80075d7e0cb4e70f77ed93c5816ea566cd
wangjing <1915093132@qq.com>
2018-08-29 16:02:30 +0800

    boss历史绩效代码提交

--- src/routes/History/Boss/index.jsx
+++ src/routes/History/Boss/index.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import MonthlyChart from 'container/MonthlyChart';
+import Funnel from 'components/Charts/FunnelCharts/Funnel';
+import styles from './index.less';
 
-export default class HistoryBoss extends React.Component {
+class HistoryBoss extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeVal: '2018.07',
+    };
+  }
+
+  toLevelPage = () => {
+    this.props.setRouteUrlParams('/level');
+  };
+
   render() {
-    return <div>333</div>;
+    const { timeVal } = this.state;
+    const aa = timeVal.split('.');
+    const FunnelChartData = [
+      { val: 30, type: 1 },
+      { val: 10, type: 2 },
+      { val: 10, type: 3 },
+      { val: 70, type: 4 },
+    ];
+    return (
+      <div>
+        <p className={styles.u_pCls}>
+          {aa[0]}年{aa[1]}月
+        </p>
+        <p className={styles.u_spanWorld}>* 实发金额以财务部税后实发为准</p>
+        <MonthlyChart
+          toLevelPage={() => {
+            this.toLevelPage();
+          }}
+        >
+          <Funnel dataSource={{ data: FunnelChartData, title: '预测绩效分档（家族）' }} />
+        </MonthlyChart>
+        <MonthlyChart
+          toLevelPage={() => {
+            this.toLevelPage();
+          }}
+        >
+          <Funnel dataSource={{ data: FunnelChartData, title: '预测绩效分档（小组）' }} />
+        </MonthlyChart>
+      </div>
+    );
   }
 }
+export default HistoryBoss;
