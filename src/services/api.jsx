49c78ded249c08c9af41e993b661d7473587578b
mzh <manzhenhua@sunlands.com>
2018-09-10 16:43:34 +0800

    修改回调地址

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -4,24 +4,24 @@ import config from '../config';
 
 const { NODE_ENV = 'pro' } = config;
 const hostObj = {
-    pro: 'http://bi-admin.ministudy.com',
-    dev: 'http://172.16.117.65:8082',
-  };
-  const HOST=hostObj[NODE_ENV];
+  pro: 'http://bi-admin.ministudy.com',
+  dev: 'http://172.16.117.65:8082',
+};
+const HOST = hostObj[NODE_ENV];
 
 /*
 *此接口为获取微信授权接口(微信企业号)
 */
-  export function getWeChart(){
-      const weChartUrlObj={
-          dev:'http://172.16.117.65:8087/authorize/RedirectToWechat?branch=dev',
-          pro:'http://bi-wechat.ministudy.com/authorize/RedirectToWechat?branch=pro',
-      }
-      return weChartUrlObj[NODE_ENV];
-  }
-  export async function getOrgMap(params){
-    return request(`${HOST}/organization/findOrgMap?${stringify(params)}`, {
-        method: 'GET',
-        body: params,
-      });
-  }
+export function getWeChart() {
+  const weChartUrlObj = {
+    dev: 'http://172.16.117.65:8087/authorizeKPI/RedirectToWechat?branch=dev',
+    pro: 'http://bi-wechat.ministudy.com/authorize/RedirectToWechat?branch=pro',
+  };
+  return weChartUrlObj[NODE_ENV];
+}
+export async function getOrgMap(params) {
+  return request(`${HOST}/organization/findOrgMap?${stringify(params)}`, {
+    method: 'GET',
+    body: params,
+  });
+}
