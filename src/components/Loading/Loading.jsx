2c82cb588edc6011970f21e5f43b3048ad4df07d
wangjing <1915093132@qq.com>
2018-09-18 17:17:58 +0800

    loading修改

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,7 +4,16 @@ import styles from './loading.css';
 
 class Loading extends React.Component {
   render() {
-    return <ActivityIndicator toast className={styles} text size="small" color="white" animating />;
+    return (
+      <ActivityIndicator
+        toast
+        className={styles}
+        text="数据加载中..."
+        size="small"
+        color="white"
+        animating
+      />
+    );
   }
 }
 export default Loading;
