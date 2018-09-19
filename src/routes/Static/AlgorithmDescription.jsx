847f5e163aab35fc697cf652dfa1f18814ef69e3
wangjing <1915093132@qq.com>
2018-09-19 17:24:23 +0800

    添加算法说明

--- src/routes/Static/AlgorithmDescription.jsx
+++ src/routes/Static/AlgorithmDescription.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import styles from './staticPage.less';
-import UseCourse1 from '../../assets/cours1.png';
-import UseCourse2 from '../../assets/cours2.png';
-import UseCourse3 from '../../assets/cours3.png';
-import UseCourse4 from '../../assets/cours4.png';
-import UseCourse5 from '../../assets/cours5.png';
-import UseCourse6 from '../../assets/cours6.png';
-import UseCourse7 from '../../assets/cours7.png';
+import formula1 from '../../assets/formula1.png';
+import formula2 from '../../assets/formula2.png';
+import formula3 from '../../assets/formula3.png';
+import formula4 from '../../assets/formula4.png';
+import formula5 from '../../assets/formula5.png';
+import formula6 from '../../assets/formula6.png';
+import formula7 from '../../assets/formula7.png';
+import formula8 from '../../assets/formula8.png';
+import formula9 from '../../assets/formula9.png';
+import formula10 from '../../assets/formula10.png';
+import formula11 from '../../assets/formula11.png';
+import formula12 from '../../assets/formula12.png';
+import formula13 from '../../assets/formula13.png';
+import formula14 from '../../assets/formula14.png';
+import formula15 from '../../assets/formula15.png';
 
 class AlgorithmDescription extends Component {
   scrollToAnchor = anchorName => {
@@ -23,15 +31,23 @@ class AlgorithmDescription extends Component {
   render() {
     return (
       <div className={styles.usercourse}>
-        <img src={UseCourse1} alt="UseCourse1" />
+        <img src={formula1} alt="formula1" />
         <a className={styles.aTest} onClick={() => this.scrollToAnchor('test')}>
-          <img className="testclass" id="smootha" src={UseCourse2} alt="UseCourse2" />
+          <img className="testclass" id="smootha" src={formula2} alt="formula2" />
         </a>
-        <img src={UseCourse3} alt="UseCourse3" />
-        <img src={UseCourse4} alt="UseCourse4" />
-        <img id="test" src={UseCourse5} alt="UseCourse5" />
-        <img src={UseCourse6} alt="UseCourse6" />
-        <img src={UseCourse7} alt="UseCourse7" />
+        <img src={formula3} alt="formula3" />
+        <img src={formula4} alt="formula4" />
+        <img id="test" src={formula5} alt="formula5" />
+        <img src={formula6} alt="formula6" />
+        <img src={formula7} alt="formula7" />
+        <img src={formula8} alt="formula8" />
+        <img src={formula9} alt="formula9" />
+        <img src={formula10} alt="formula10" />
+        <img src={formula11} alt="formula11" />
+        <img src={formula12} alt="formula12" />
+        <img src={formula13} alt="formula13" />
+        <img src={formula14} alt="formula14" />
+        <img src={formula15} alt="formula15" />
       </div>
     );
   }
