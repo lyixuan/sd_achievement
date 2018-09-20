3a716ec118b3c7718ee53137df18d50c5a955312
mzh <manzhenhua@sunlands.com>
2018-09-20 15:14:12 +0800

    修改文案

--- src/components/Charts/BarCharts/proportion.js
+++ src/components/Charts/BarCharts/proportion.js
@@ -15,7 +15,7 @@ export class Proportion extends BarClass {
     const str = `<div><span style="display: inline-block"><a style="width:0.08rem;height: 0.08rem;margin-right:0.1rem;color:${
       params[0].color
     }">●</a>${name}: ${value}%</span><br />`;
-    const secondRow = `小组数 ${selectedObj.num || 0}  小组绩效: ${this.breakNumComma(
+    const secondRow = `小组数: ${selectedObj.num || 0}  小组绩效: ${this.breakNumComma(
       selectedObj.collegeAchievement
     ) || 0}元`;
     return str + secondRow;
