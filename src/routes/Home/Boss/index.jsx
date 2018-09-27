0b5893080bc8cd7077a5b6d924d75a28ad1640f5
mzh <manzhenhua@sunlands.com>
2018-09-27 10:46:52 +0800

    修改切换权限返回异常的问题

--- src/routes/Home/Boss/index.jsx
+++ src/routes/Home/Boss/index.jsx
@@ -3,9 +3,12 @@ import { connect } from 'dva';
 import { Redirect, Switch, Route } from 'dva/router';
 import SelfTab from 'components/SelfTab/SelfTab';
 import { timeArea } from 'utils/timeArea';
+import { getCurrentAuthInfo, currentPathName } from 'utils/decorator';
 import { getRoutes, assignUrlParams } from '../../../utils/routerUtils';
 import styles from './index.less';
 
+@getCurrentAuthInfo
+@currentPathName
 class Boss extends React.Component {
   constructor(props) {
     super(props);
@@ -15,6 +18,12 @@ class Boss extends React.Component {
       month: maxDate,
     };
     this.state = assignUrlParams(initState, urlParams);
+  }
+  componentDidMount() {
+    const pathname = this.checkoutUserAuthPathName(); // 检测用户权限,如果该权限不能调转到该页面的话则跳转到指定页面
+    if (pathname !== '/indexPage/boss') {
+      this.props.setRouteUrlParams(pathname, {});
+    }
   }
   changePage(id) {
     const { pathname } = this.props.location;
