29d11206a8cff6a612256cc498d889a2c93a7806
mzh <manzhenhua@sunlands.com>
2019-01-25 11:30:51 +0800

    修改最小月

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   NODE_ENV: 'dev', // 测试环境
   // NODE_ENV: 'pro', // 线上环境
 
-  DEBUGGER: true, // 调试环境
+  DEBUGGER: false, // 调试环境
   performanceUser: {
     userId: 'ManZhenHua',
   },
