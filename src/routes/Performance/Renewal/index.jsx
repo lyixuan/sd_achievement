c80b2846e336826a3d13d99c5e2de5d8bb8d5eca
wenwanyu <wenwanyu@sunlands.com>
2019-08-20 19:56:56 +0800

    完善页面

--- src/routes/Performance/Renewal/index.jsx
+++ src/routes/Performance/Renewal/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Icon } from 'antd-mobile';
 import { getCurrentAuthInfo, getCurrentMonth } from 'utils/decorator';
 import DatePanle from 'container/DatePanle';
 import Table from '../component/table';
@@ -11,6 +12,7 @@ class Renewal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: 0,
       month: this.currentMonth(),
       // collegeId,
       // userId,
@@ -40,9 +42,28 @@ class Renewal extends React.Component {
   };
 
   render() {
-    const { month } = this.state;
+    const { month, id } = this.state;
     const { findRenewalKpiDetailData } = this.props.performance;
+    const showFirstId = findRenewalKpiDetailData.length && findRenewalKpiDetailData[0].itemId;
     const columnsData = [
+      {
+        title: '报名日期',
+        dataIndex: 'registrationDate',
+        key: 'registrationDate',
+      },
+      {
+        title: '子订单ID',
+        dataIndex: 'subOrderId',
+        key: 'subOrderId',
+      },
+      {
+        title: '净流水(元)',
+        dataIndex: 'financeNetFlow',
+        key: 'financeNetFlow',
+      },
+    ];
+
+    const columnsDataMeta = [
       {
         title: '岗位',
         dataIndex: 'positionType',
@@ -93,7 +114,47 @@ class Renewal extends React.Component {
           <div className={styles.middle}>
             <p>续报绩效 = 续报净流水 x 岗位提点</p>
           </div>
-          {<Table columnsData={columnsData} rowData={findRenewalKpiDetailData} />}
+          <div className={styles.presidentContent}>
+            <p className={styles.meta}>
+              {columnsDataMeta.map(item => {
+                return <span key={item.title}>{item.title}</span>;
+              })}
+            </p>
+            <ul className={styles.list}>
+              {findRenewalKpiDetailData.map(item => {
+                return (
+                  <li key={item.positionType}>
+                    <div className={styles.items}>
+                      <span>{item.positionType}</span>
+                      <span>{item.positionDistribution}</span>
+                      <span>{item.totalKpi}</span>
+                      <span
+                        onClick={() => this.toggle(item.itemId)}
+                        style={{
+                          alignItems: 'center',
+                          width: '10%',
+                          display: 'flex',
+                          justifyContent: 'center',
+                          cursor: 'pointer',
+                        }}
+                      >
+                        <Icon
+                          type={(id || showFirstId) === item.itemId ? 'up' : 'down'}
+                          size="xs"
+                          color="#00ccc3"
+                        />
+                      </span>
+                    </div>
+                    {(id || showFirstId) === item.itemId && (
+                      <Table columnsData={columnsData} rowData={item.renewalOrderList} />
+                      // <ul className={styles.list1}>{this.renderIem1(item.renewalOrderList)}</ul>
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          {/* {<Table columnsData={columnsData} rowData={findRenewalKpiDetailData} />} */}
         </div>
       </div>
     );
