aa70cb89e712e0ff60501c4407db4e2966721048
lyx <123>
2019-08-22 19:31:08 +0800

    改为调试模式

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-  // NODE_ENV: 'dev', // 测试环境
-  NODE_ENV: 'pro', // 线上环境
+  NODE_ENV: 'dev', // 测试环境
+  // NODE_ENV: 'pro', // 线上环境
 
-  DEBUGGER: false, // 调试环境
+  DEBUGGER: true, // 调试环境
   performanceUser: {
     userId: 'liuyixuan02',
   },
